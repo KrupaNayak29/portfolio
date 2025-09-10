e8204cbcab24bdb4187c0e9be7fd5e32b20df747
Krupa Nayak <nayakkrupa69@gmail.com>
2025-09-10 07:32:09 +0530

    updated portfolio

--- src/main/java/in/krupanayak/portfolio/controller/HomeController.java
+++ src/main/java/in/krupanayak/portfolio/controller/HomeController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping({"","/","/home"})
-    public String showHomePage(){
-        return "home";
+    @GetMapping({"", "/", "/home"})
+    public String showHomePage() {
+        return "index"; // Spring will render index.html from templates folder
     }
-
 }
+
 
 
